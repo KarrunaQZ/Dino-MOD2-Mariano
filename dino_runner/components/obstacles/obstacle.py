9a8f4826b8279b6c22e1e4295255d9c8aec1418f
KarrunaQZ <KarrunaQZ@users.noreply.github.com>
2023-05-02 21:31:13 -0300

    Aula dia 02/05

---
+++ dino_runner/components/obstacles/obstacle.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.sprite import Sprite
+from dino_runner.utils.constants import SCREEN_WIDTH
+
+
+class Obstacle(Sprite):
+    def __init__(self, image, type):
+        pass
+    def update(self, game_speed, obstacles):
+        pass
+    def draw(self, screen):
+        pass
---
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+
+from dino_runner.components.obstacles.cactus import Cactus
+from dino_runner.components.obstacles.bird import Bird
+
+
+class ObstacleManager:
+    def __init__(self):
+        pass
+    def update(self, game):
+        pass
+    def reset_obstacles(self):
+        pass
+    def draw(self, screen):
+        pass
